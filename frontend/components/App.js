68e0f37af6796463a66661bc09f07c1227abc950
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-20 14:44:01 -0400

    chore: Add express-rate-limit npm dependency for rate limiting

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react' // 👈 you'll need the reducer hook
-import Quotes from './Quotes'
-import QuoteForm from './QuoteForm'
+import React from "react"; // 👈 you'll need the reducer hook
+import Quotes from "./Quotes";
+import QuoteForm from "./QuoteForm";
 
 // 👇 these are the types of actions that can change state
-const CREATE_QUOTE = 'CREATE_QUOTE'
-const DELETE_QUOTE = 'DELETE_QUOTE'
-const EDIT_QUOTE_AUTHENTICITY = 'EDIT_QUOTE_AUTHENTICITY' // 👈 toggles the apocryphal property of a single quote
-const SET_HIGHLIGHTED_QUOTE = 'SET_HIGHLIGHTED_QUOTE'     // 👈 highlights a quote (or un-highlights it)
-const TOGGLE_VISIBILITY = 'TOGGLE_VISIBILITY'             // 👈 toggles whether to show all or only non-apocryphal
+const CREATE_QUOTE = "CREATE_QUOTE";
+const DELETE_QUOTE = "DELETE_QUOTE";
+const EDIT_QUOTE_AUTHENTICITY = "EDIT_QUOTE_AUTHENTICITY"; // 👈 toggles the apocryphal property of a single quote
+const SET_HIGHLIGHTED_QUOTE = "SET_HIGHLIGHTED_QUOTE"; // 👈 highlights a quote (or un-highlights it)
+const TOGGLE_VISIBILITY = "TOGGLE_VISIBILITY"; // 👈 toggles whether to show all or only non-apocryphal
 
-let id = 1
-const getNextId = () => id++ // 👈 this is a helper to create new quotes
+let id = 1;
+const getNextId = () => id++; // 👈 this is a helper to create new quotes
 const quotes = [
   {
     id: getNextId(),
@@ -30,13 +30,13 @@ const quotes = [
     authorName: "Oscar Wilde",
     apocryphal: false,
   },
-]
+];
 
 // 👇 create your initial state object here
 
 const reducer = (state, action) => {
   // 👇 implement your reducer here using the action types above
-}
+};
 
 export default function App() {
   // 👇 use the reducer hook to spin up state and dispatch
@@ -44,31 +44,28 @@ export default function App() {
   const createQuote = ({ authorName, quoteText }) => {
     // 👇 use the helper function above to create a new quote
     // 👇 and dispatch it over to the reducer
-  }
-  const deleteQuote = id => {
+  };
+  const deleteQuote = (id) => {
     // 👇 implement
-  }
-  const editQuoteAuthenticity = id => {
+  };
+  const editQuoteAuthenticity = (id) => {
     // 👇 implement
-  }
-  const setHighlightedQuote = id => {
+  };
+  const setHighlightedQuote = (id) => {
     // 👇 implement
-  }
+  };
   const toggleVisibility = () => {
     // 👇 implement
-  }
+  };
 
   return (
     <div id="mp">
       <h2>Module Project</h2>
       <Quotes
         quotes={quotes}
-      // 👇 lots of props are missing! Check the Quotes component
-
+        // 👇 lots of props are missing! Check the Quotes component
       />
-      <QuoteForm
-        createQuote={createQuote}
-      />
+      <QuoteForm createQuote={createQuote} />
     </div>
-  )
+  );
 }
--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,52 +1,53 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React from "react"; // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
-const CHANGE_INPUT = 'CHANGE_INPUT'
-const RESET_FORM = 'RESET_FORM'
+const CHANGE_INPUT = "CHANGE_INPUT";
+const RESET_FORM = "RESET_FORM";
 
 // 👇 create your initial state object here
 
 // 👇 create your reducer function here
 
-export default function TodoForm({ createQuote = () => { } }) {
+export default function TodoForm({ createQuote = () => {} }) {
   // 👇 use the reducer hook to spin up state and dispatch
 
   const onChange = () => {
     // 👇 implement
-  }
+  };
   const resetForm = () => {
     // 👇 implement
-  }
+  };
   const onNewQuote = () => {
     // 👇 implement
-    resetForm()
-  }
+    resetForm();
+  };
 
   // 👇 some props are missing in the JSX below:
   return (
     <form id="quoteForm" onSubmit={onNewQuote}>
       <h3>New Quote Form</h3>
-      <label><span>Author:</span>
+      <label>
+        <span>Author:</span>
         <input
-          type='text'
-          name='authorName'
-          placeholder='type author name'
+          type="text"
+          name="authorName"
+          placeholder="type author name"
           onChange={onChange}
         />
       </label>
-      <label><span>Quote text:</span>
+      <label>
+        <span>Quote text:</span>
         <textarea
-          type='text'
-          name='quoteText'
-          placeholder='type quote'
+          type="text"
+          name="quoteText"
+          placeholder="type quote"
           onChange={onChange}
         />
       </label>
-      <label><span>Create quote:</span>
-        <button
-          role='submit'
-        >DO IT!</button>
+      <label>
+        <span>Create quote:</span>
+        <button role="submit">DO IT!</button>
       </label>
     </form>
-  )
+  );
 }
